c7db36db43eb9bf21ad7c1d4f1a2fc535a279873
Thiago <thiago.w3c@gmail.com>
2024-03-10 01:55:40 +0000

    Function Currying Using bind()

---
+++ currying-bind.js
@@ -0,0 +1,20 @@
+// Function currying is a technique in JavaScript where a function with multiple arguments is transformed into a sequence of functions,
+// each with a single argument. This can be achieved using the bind() method.
+
+function myPet(petName, favoriteFood) {
+  return `My pet's name is ${petName} and they love to eat ${favoriteFood}`;
+}
+
+let myPetDescription = myPet.bind(this, 'Foguinho');
+console.log(myPetDescription('Chocolate'));
+console.log(myPetDescription('Pasta'));
+
+
+function makePetDescription(name, food, age) {
+    return `My pet's name is ${name}, he likes to eat ${food}, and he is ${age} years old.`;
+}
+
+let myPetDescription2 = makePetDescription.bind(this, 'Gelinho', 'Chocolate');
+console.log(myPetDescription2(3));
+console.log(myPetDescription2(4));
+
