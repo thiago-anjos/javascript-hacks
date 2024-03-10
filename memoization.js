468b030f0ea5f36d07d0b2094f46519389917147
Thiago <thiago.w3c@gmail.com>
2024-03-10 02:48:46 +0000

    Memoization for Performance Optimization

---
+++ memoization.js
@@ -0,0 +1,31 @@
+console.time('fibonacci');
+
+function fibonacci(n) {
+if (n <= 1) {
+    return n;
+} else {
+    //console.log(n)
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+}
+
+console.log('no memoization:', fibonacci(5)); // Output: 5
+
+console.timeEnd('fibonacci');
+
+
+console.time('memoization');
+function memoize(func) {
+    const cache = new Map();
+    return function(n) {
+      if (!cache.has(n)) {
+        cache.set(n, func(n));
+      }
+      return cache.get(n);
+    };
+  }
+  
+  const memoizedFibonacci = memoize(fibonacci);
+  console.log('memoized:', memoizedFibonacci(5));
+
+  console.timeEnd('memoization')
