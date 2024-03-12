0fc7e331bf1017e45aab0289ade09f7c57d521d9
Thiago <thiago.w3c@gmail.com>
2024-03-12 01:27:44 +0000

    using useReducer

---
+++ app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { useReducer } from 'react';
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action){
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <h1>Count: {state.count}</h1>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
+  );
+}
+
+export default App;
--- new-set.js
+++
@@ -1,5 +0,0 @@
-// new set to remove duplicate values from array
-
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let uniqueNumbers = [...new Set(numbers)];
-console.log(uniqueNumbers);
