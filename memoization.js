a69fefcf84cc3e7ccecf4b1d2edcd5d02a855368
thiago <thiago.w3c@gmail.com>
2024-05-30 23:22:16 -0300

    feat: 🎸 add biome to project
    seams like linter and prettier but in one package

--- memoization.js
+++ memoization.js
@@ -1,31 +1,30 @@
-console.time('fibonacci');
+console.time("fibonacci");
 
 function fibonacci(n) {
-if (n <= 1) {
+  if (n <= 1) {
     return n;
-} else {
+  } else {
     //console.log(n)
     return fibonacci(n - 1) + fibonacci(n - 2);
-}
-}
-
-console.log('no memoization:', fibonacci(5)); // Output: 5
-
-console.timeEnd('fibonacci');
-
-
-console.time('memoization');
-function memoize(func) {
-    const cache = new Map();
-    return function(n) {
-      if (!cache.has(n)) {
-        cache.set(n, func(n));
-      }
-      return cache.get(n);
-    };
   }
-  
-  const memoizedFibonacci = memoize(fibonacci);
-  console.log('memoized:', memoizedFibonacci(5));
+}
 
-  console.timeEnd('memoization')
+console.log("no memoization:", fibonacci(5)); // Output: 5
+
+console.timeEnd("fibonacci");
+
+console.time("memoization");
+function memoize(func) {
+  const cache = new Map();
+  return function (n) {
+    if (!cache.has(n)) {
+      cache.set(n, func(n));
+    }
+    return cache.get(n);
+  };
+}
+
+const memoizedFibonacci = memoize(fibonacci);
+console.log("memoized:", memoizedFibonacci(5));
+
+console.timeEnd("memoization");
