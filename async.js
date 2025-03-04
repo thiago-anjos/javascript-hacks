c7c00d5360508acd153d3385cb701a9f5679476a
Thiago <thiago.w3c@gmail.com>
2025-03-04 18:42:49 +0000

    examples async

--- async.js
+++ async.js
@@ -2,7 +2,6 @@ const myPromisse = function(time){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             const random = Math.random();
-            console.log("random: ", random);
             if (random > 0.5) {
                 resolve("Success!");
             } else {
@@ -16,28 +15,63 @@ const promiseOne = myPromisse.bind(this, 1000);
 const promiseTwo = myPromisse.bind(this, 2000);
 const promiseThree = myPromisse.bind(this, 3000);
 
-promiseOne()
-    .then((result) => {
-        console.log("Promise One:", result);
-        return promiseTwo();
-    })
-    .catch((error) => {
+// promiseOne()
+//     .then((result) => {
+//         console.log("Promise One:", result);
+//     })
+//     .catch((error) => {
+//         console.error("Promise One Error:", error);
+// });
+
+// promiseTwo()
+//     .then((result) => {
+//         console.log("Promise Two:", result);
+//     })
+//     .catch((error) => {
+//         console.error("Promise Two Error:", error);
+// });
+
+// promiseThree()
+//     .then((result) => {
+//         console.log("Promise Three:", result);
+//     })
+//     .catch((error) => {
+//         console.error("Promise Three Error:", error);
+// });
+
+// Promise.allSettled([promiseOne(), promiseTwo(), promiseThree()])
+//     .then((results) => {
+//         console.log("All promises resolved:", results);
+//     })
+//     .catch((error) => {
+//         console.error("One or more promises failed:", error);
+//     });
+
+
+// sintaxe mais legivel e mais moderna async await
+
+const myAsyncFunction = async function(){
+    try {
+        const resultOne = await promiseOne();
+        console.log("Promise One:", resultOne);
+    } catch (error) {
         console.error("Promise One Error:", error);
-});
+    }
 
-promiseTwo()
-    .then((result) => {
-        console.log("Promise Two:", result);
-        return promiseThree();
-    })
-    .catch((error) => {
+    try {
+        const resultTwo = await promiseTwo();
+        console.log("Promise Two:", resultTwo);
+    } catch (error) {
         console.error("Promise Two Error:", error);
-});
+    }
 
-promiseThree()
-    .then((result) => {
-        console.log("Promise Three:", result);
-    })
-    .catch((error) => {
+    try {
+        const resultThree = await promiseThree();
+        console.log("Promise Three:", resultThree);
+    } catch (error) {
         console.error("Promise Three Error:", error);
-});
+    }
+}
+
+myAsyncFunction();
+
